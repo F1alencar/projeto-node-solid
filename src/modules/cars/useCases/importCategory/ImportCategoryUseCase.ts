2d7ca6d9b0f58d2e904e4f1cfc0af44c6780da53
F1alencar <felype99azevedo@gmail.com>
2022-08-27 21:45:07 -0300

    feat: adicionado swagger na aplicação e remoção de arquivos

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -29,6 +29,7 @@ class ImportCategoryUseCase {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           return resolve(categories);
         })
         .on("error", (err) => {
