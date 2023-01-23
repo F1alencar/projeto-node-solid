e60f0207953a2a4a49c0e5117393d63c668dc8bf
F1alencar <felype99azevedo@gmail.com>
2023-01-23 14:04:14 -0300

    feat: adicionado testes unitários nos casos de usos "CreateCategory" e "AuthenticateUser"

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,33 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category | null> {
+    const category =
+      this.categories.find((category) => category.name === name) || null;
+
+    return category;
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export { CategoriesRepositoryInMemory };
